b4aea69841c2a15aac78b4b7e5bec863a7cd40cb
Ashula <ashula@users.noreply.github.com>
2017-10-24 04:53:13 +0900

    nx_get_images.py Ver-0.03
    define function nx_get_images(VIP)
       VIP: traget NX cluster VIP & PRISM PORT.
          e.x. 172.16.2.109:9440

--- nx_get_images.py
+++ nx_get_images.py
@@ -1,6 +1,7 @@
 #
 #    Module: nx_get_images.py
 #
+#    Ver-0.03: 24Oct2017, define nx_get_images()
 #    Ver-0.02: 10Oct2017, text -> json conversion should be done in nx_rest_api.rest_api().
 #    Ver-0.01: 20Oct2017, Initial implementation from nx_images.py and nx_rest_api.py
 #
@@ -16,15 +17,20 @@ import requests
 import read_credentials
 import nx_rest_api
 
+def  nx_get_images(VIP):
+#    VIP = '172.16.11.109:9440'
+    SUB_URL = 'PrismGateway/services/rest/v2.0/images/'
+
+    payload = {}
+    r = requests.Response()
+    r = nx_rest_api.rest_api(VIP, SUB_URL, payload, 'get')
+    return r
+
 
 if (__name__=='__main__'):
-    VIP='172.16.11.109:9440'
-    SUB_URL='PrismGateway/services/rest/v2.0/images/'
-    
-    payload={}
-    r = requests.Response()
-    r = nx_rest_api.rest_api(VIP,SUB_URL,payload,'get')
+    VIP = '172.16.11.109:9440'
+    r=nx_get_images(VIP)
 
-#    print r.status_code
-    print r.text
+    print "Return_code:%d" % r.status_code
+    print "%s" % r.text
 
