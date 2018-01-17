878fdd781eb032f023f1b61d10a69ae54004be3a
Ashula <ashula@users.noreply.github.com>
2018-01-18 03:51:23 +0900

    nx_get_vms.py ver-0.03, 17Jan2018
    17Jan2018
    get VM info from target NX cluster.
    print out VM_name, Storage_Container_uuid, vmdisk_uuid, ip_address(if assigned) in CSV format.

--- nx_get_vms.py
+++ nx_get_vms.py
@@ -1,6 +1,7 @@
 #
 #    Module: nx_get_vms.py
 #
+#    Ver-0.03: 17Jan2018, get vm info with nic and vmdisk info.
 #    Ver-0.02: 21Dec2017, get vm info with nic info.
 #    Ver-0.01: 05Nov2017, initial nx_get_vms.py from nx_get_image.py Ver-0.05.
 #    Ver-0.05: 25Oct2017, get VIP through arguments.
@@ -11,7 +12,8 @@
 #
 #    $ python ./nx_get_vms.py  VIP 
 #
-#    return REST api
+#    func. nx_get_vms() return REST api
+#    func. print_nx_vms_list() prints list of VM_name, container_uuid, vmdisk_uuid, ip_address
 #
 import os
 import sys
@@ -22,24 +24,50 @@ import get_argv
 import read_credentials
 import nx_rest_api
 
-def  nx_get_hosts(VIP):
+def  nx_get_vms(VIP):
 #    SUB_URL = 'PrismGateway/services/rest/v2.0/images/'
-    SUB_URL = 'api/nutanix/v2.0/vms/?include_vm_nic_config=true'
+    SUB_URL = 'api/nutanix/v2.0/vms/?include_vm_disk_config=true&include_vm_nic_config=true'
 
     payload = {}
     r = requests.Response()
     r = nx_rest_api.rest_api(VIP, SUB_URL, payload, 'get')
     return r
 
+def print_nx_vms_list(JSON):
+    # for some reasons, 'total_entities' = 0 in AOS-5.1.0.3
+    num = JSON["metadata"]["count"]
+    print num
+
+    i = 0
+    while (i < num):
+        vm = JSON["entities"][i]
+        # print >>sys.stderr,"%s:%s"  % (vm["name"],image["uuid"])
+        num_disk = (len(vm['vm_disk_info']))
+        num_nic  = (len(vm['vm_nics']))
+
+        # print "%s, disks=%d, nics=%d" % (vm['name'], num_disk, num_nic)
+
+        if num_nic == 0 :
+            print "%s,%s,%s," %  (vm['name'],vm['vm_disk_info'][1]['storage_container_uuid'],vm['vm_disk_info'][1]['disk_address']['vmdisk_uuid'])
+        else:
+            exist_ip = vm['vm_nics'][0].has_key('requested_ip_address')
+            if exist_ip == True:
+                print "%s,%s,%s,%s" %  (vm['name'],vm['vm_disk_info'][1]['storage_container_uuid'],vm['vm_disk_info'][1]['disk_address']['vmdisk_uuid'],vm['vm_nics'][0]['requested_ip_address'])
+            else:
+                print "%s,%s,%s," %  (vm['name'],vm['vm_disk_info'][1]['storage_container_uuid'],vm['vm_disk_info'][1]['disk_address']['vmdisk_uuid'])
+
+        i += 1
+
 
 if (__name__=='__main__'):
-#    VIP = '172.16.2.109'
 
     argv=get_argv.get_argv(2)
     VIP=argv[1]
 
-    r=nx_get_hosts(VIP)
+    r=nx_get_vms(VIP)
 
     print >> sys.stderr, "Return_code:%d" % r.status_code
     print "%s" % r.text
+
+    print_nx_vms_list(r.json())
 
