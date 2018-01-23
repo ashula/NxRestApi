bf4f645e0775aebd74a8ff1c7d4df4255845bc20
Ashula <ashula@users.noreply.github.com>
2018-01-24 01:31:01 +0900

    get_argv.py, Ver-0.03 to return argc, argv[] pair
    Ver-0.03: 23Jan2018, return argc, argv[]

--- get_argv.py
+++ get_argv.py
@@ -3,6 +3,7 @@
 #
 #    get argment strings into returning list
 #
+#    Ver-0.03: 23Jan2018, return argc, argv[]
 #    Ver-0.02: 25Oct2017, Check actual argc and argv.
 #    Ver-0.01: 23Oct2017, Initial implementation.
 #
@@ -14,7 +15,7 @@
 #      :
 #    arg[num_argc-1]: (num_argc -1)th argument.
 #
-#    return list of argvs.
+#    return number of args and list of argvs.
 #
 import sys
 
@@ -26,11 +27,12 @@ def get_argv(num_argc):
         print >> sys.stderr, "Error: number(%d) of argments is wrong aganst specified(%d)" % (num_argc, argc)
         print >> sys.stderr, argv
         sys.exit()
-    return argv
+    return argc, argv
 
 if (__name__=="__main__"):
-    argv = get_argv(4)
+    argc, argv = get_argv(5)
 #    print >> sys.stderr, argv
+    print "argc=%s" % argc
     i=0
     for x in argv[0:]:
         print "argv[%s]=%s" % (i, x)
