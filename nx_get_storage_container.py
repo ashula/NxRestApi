21f6208175f6ab30f9b068928c3abe4fb33c7d01
Ashula <ashula@users.noreply.github.com>
2018-01-24 01:38:10 +0900

    nx_get_storage_container.py, Ver-0.02: 23Jan2018
    Change to get_args.py to get argc too.

---
+++ nx_get_storage_container.py
@@ -0,0 +1,96 @@
+#
+#    Module: nx_get_storage_container.py
+#
+#    Ver-0.01: 19Jan2018, Initial implementation to get storage_container info.  from nx_get_vmdisk.py
+#    Ver-0.01: 18Jan2018, Initial implementation to get vmdisk info .  from nx_get_vms.py
+#    Ver-0.03: 17Jan2018, get vm info with nic and vmdisk info.
+#    Ver-0.02: 21Dec2017, get vm info with nic info.
+#    Ver-0.01: 05Nov2017, initial nx_get_vms.py from nx_get_image.py Ver-0.05.
+#    Ver-0.05: 25Oct2017, get VIP through arguments.
+#    Ver-0.04: 25Oct2017, separate port address of PRISM from VIP argument. (due to change of rest_api())
+#    Ver-0.03: 24Oct2017, define nx_get_images()
+#    Ver-0.02: 10Oct2017, text -> json conversion should be done in nx_rest_api.rest_api().
+#    Ver-0.01: 20Oct2017, Initial implementation from nx_images.py and nx_rest_api.py
+#
+#    $ python ./nx_get_vmdisk.py  VIP VM_Name
+#
+#    func. nx_get_storage_container()
+#    func. nx_get_storage_container_by_uuid()
+#    func. print_nx_vms_list() prints list of VM_name, container_uuid, vmdisk_uuid, ip_address
+#
+import os
+import sys
+import traceback
+import json
+import requests
+import get_argv
+import read_credentials
+import nx_rest_api
+
+#def  nx_get_vms(VIP):
+##    SUB_URL = 'PrismGateway/services/rest/v2.0/images/'
+#    SUB_URL = 'api/nutanix/v2.0/vms/?include_vm_disk_config=true&include_vm_nic_config=true'
+#
+#    payload = {}
+#    r = requests.Response()
+#    r = nx_rest_api.rest_api(VIP, SUB_URL, payload, 'get')
+#    return r
+
+def nx_get_storage_containers(VIP):
+    SUB_URL = "/api/nutanix/v2.0/storage_containers/"
+
+    payload = {}
+    r = requests.Response()
+    r = nx_rest_api.rest_api(VIP, SUB_URL, payload, 'get')
+    return r
+
+def nx_get_storage_container_by_uuid(VIP, UUID):
+    SUB_URL = "https://172.16.8.109:9440/api/nutanix/v2.0/storage_containers/"+UUID
+
+    payload = {}
+    r = requests.Response()
+    r = nx_rest_api.rest_api(VIP, SUB_URL, payload, 'get')
+    return r
+
+def print_nx_storage_uuid(VID):
+    # for some reasons, 'total_entities' = 0 in AOS-5.1.0.3
+    num = JSON["metadata"]["count"]
+    # print num
+
+    i = 0
+    while (i < num):
+        vm = JSON["entities"][i]
+        # print >>sys.stderr,"%s:%s"  % (vm["name"],image["uuid"])
+        num_disk = (len(vm['vm_disk_info']))
+        num_nic  = (len(vm['vm_nics']))
+
+        # print "%s, disks=%d, nics=%d" % (vm['name'], num_disk, num_nic)
+
+        if num_nic == 0 :
+            print "%s,%s,%s," %  (vm['name'],vm['vm_disk_info'][1]['storage_container_uuid'],vm['vm_disk_info'][1]['disk_address']['vmdisk_uuid'])
+        else:
+            exist_ip = vm['vm_nics'][0].has_key('requested_ip_address')
+            if exist_ip == True:
+                print "%s,%s,%s,%s" %  (vm['name'],vm['vm_disk_info'][1]['storage_container_uuid'],vm['vm_disk_info'][1]['disk_address']['vmdisk_uuid'],vm['vm_nics'][0]['requested_ip_address'])
+            else:
+                print "%s,%s,%s," %  (vm['name'],vm['vm_disk_info'][1]['storage_container_uuid'],vm['vm_disk_info'][1]['disk_address']['vmdisk_uuid'])
+
+        i += 1
+
+
+if (__name__=='__main__'):
+
+    argc, argv=get_argv.get_argv(3)
+    VIP=argv[1]
+    UUID=argv[2]
+
+    r=nx_get_storage_containers(VIP)
+
+    print >> sys.stderr, "Return_code:%d" % r.status_code
+    print "%s" % r.text
+
+    print "="*40
+
+    r=nx_get_storage_container_by_uuid(VIP,UUID)
+    print "%s" % r.text
+
