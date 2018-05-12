cbd8008c407b9c9022a82370997fa53ff236cf68
Ashula <ashula@users.noreply.github.com>
2018-05-13 05:27:37 +0900

    pg_select.py
    Connect Postgresql from python by using psycopg2 module.
    This example uses just SQL SELECT statement.

---
+++ pg_select.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF8
+#
+#  pg_select.py: run SQL SELECT of Postgresql from python 
+#  
+#  Ver-0.01, 20180513: initial implementation.
+#
+#      using psycopg2 module to connect to PostgreSQL server.
+#
+import psycopg2
+import psycopg2.extensions
+
+DSN = "dbname='NxJapanSeSme' user='postgres' host='172.16.251.34' password='Pg2018tm'"
+conn = psycopg2.connect(DSN)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED)
+conn.set_client_encoding("UTF8")
+cur = conn.cursor()
+
+cur.execute("SELECT se_key, se_full_name_eng, se_full_name_jp from se_t")
+rows = cur.fetchall()
+for row in rows:
+    print row[0], row[1], row[2]
+
+conn.commit()
+
+cur.execute("COMMIT")
+cur.close
+conn.commit
+
